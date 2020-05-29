9257794dd14b4beaaf9165c1d98b35cd673d42bf
francisco28 <fgallegosgo@gmail.com>
2020-05-29 17:51:15 -0600

    add css to the phrase and finalizing app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import Phrase from './components/Phrase';
 
 const Container = styled.div`
     display: flex;
     align-items: center;
-    padding-top: 5rem;
+    padding-top: 3rem;
+    margin-top: 2rem;
     flex-direction: column;
 `;
 
@@ -14,10 +15,16 @@ const Button = styled.button`
     background-size: 300px;
     font-family: Arial, Helvetica, sans-serif;
     color: #fff;
-    margin-top: 3rem;
+    margin-top: 2rem;
     padding: 1rem 3rem;
     font-size: 2rem;
     border: 2px solid black;
+    transition: background-size .8s inherit;
+
+    :hover {
+      cursor: pointer;
+      background-size: 400px;
+    }
 `;
 
 function App() {
@@ -26,11 +33,17 @@ function App() {
   //state of phrases
   const [ phrase, savePhrase ] = useState({});
   
+  //API
   const consultAPI = async () => {
     const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
     const phrase = await api.json();
     savePhrase(phrase[0]);
   }
+
+  //load the api automatically
+  useEffect( () => {
+    consultAPI()
+  }, []);
 
   return (
     
--- src/components/Phrase.js
+++ src/components/Phrase.js
@@ -2,14 +2,46 @@ import React, { Fragment} from 'react';
 import styled from '@emotion/styled';
 
 const ContainerPhrase = styled.div`
-    padding-top: 5rem;
-    margin-top: 5rem;
-    width: 70%;
-    text-align: center;
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width: 800px;
+
+    @media(min-width: 992px) {
+        margin-top: 14rem;
+    }
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
 `;
 
 
 const Phrase = ({phrase}) => {
+
+    if (Object.keys(phrase).length === 0) return null;
+
     return ( 
         <ContainerPhrase>
             <h1>{phrase.quote}</h1>
